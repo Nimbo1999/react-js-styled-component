ef7db2851481996c5988ab95a3580b68f5acfa8e
Matheus Lopes <matlopes1999@gmail.com>
2021-01-22 22:23:10 -0300

    Add the product component and its styled components

--- src/components/product-cell/index.tsx
+++ src/components/product-cell/index.tsx
@@ -1,17 +1,35 @@
-import {} from './styledProductCell';
+import {
+    ProductCellWrapper, CurrentPrice, LastPrice, Row, PName, PDescription, Img
+ } from './styledProductCell';
 
 interface Props {
-
+    image?: string,
+    price: number,
+    discountprice?: number,
+    name: string,
+    description?: string,
 }
 
-const ProductCell = ({}:Props) => {
+const ProductCell = ({ image, price, discountprice, name, description }:Props) => {
 
+    // Use the male photos directory at assets to mock the images.
     return (
-        <div>
-            <span>R$ 23,00</span>
-            <span>R$ 29,99</span>
-            <small>Blusa masculina</small>
-        </div>
+        <ProductCellWrapper>
+            <div>
+                <Img src={ image } />
+            </div>
+            <Row>
+                {discountprice
+                    ? (<>
+                        <CurrentPrice>{discountprice.toLocaleString('pt-BR', {currency: 'BRL', style: 'currency'})}</CurrentPrice>
+                        <LastPrice>{price.toLocaleString('pt-BR', {currency: 'BRL', style: 'currency'})}</LastPrice>
+                    </>) : (
+                        <CurrentPrice>{price.toLocaleString('pt-BR', {currency: 'BRL', style: 'currency'})}</CurrentPrice>
+                    ) }
+            </Row>
+            <PName>{name}</PName>
+            <PDescription>{description}</PDescription>
+        </ProductCellWrapper>
     );
 }
 
--- src/components/product-cell/styledProductCell.ts
+++ src/components/product-cell/styledProductCell.ts
@@ -1,2 +1,48 @@
 import styled from 'styled-components';
 
+export const ProductCellWrapper = styled.div`
+    background-color: #E8F0F8;
+    flex: 1 0 260px;
+    max-width: 260px;
+    margin-left: 16px;
+
+    &:first-child {
+        margin-left: 0px;
+    }
+`;
+
+export const Row = styled.div`
+    display: flex;
+    justify-content: flex-start;
+    margin-top: .5em;
+    align-items: center;
+    padding: 0px 12px;
+    width: 100%;
+`;
+
+export const CurrentPrice = styled.span`
+    margin-right: 8px;
+    font-weight: bold;
+    font-size: 16px;
+`;
+
+export const LastPrice = styled.span`
+    font-size: 14px;
+    text-decoration: line-through;
+`;
+
+export const PName = styled.p`
+    padding: 8px 12px;
+    margin: 0px;
+
+`;
+
+export const PDescription = styled.small`
+    padding: 0px 12px 12px;
+    display: ${({ children }) => children ? 'block' : 'none'}
+
+`;
+
+export const Img = styled.img`
+    width: 100%;
+`;
