065aa2278fb1a920dc52813d7ef74d5dc1fb5716
Matheus Lopes <matlopes1999@gmail.com>
2021-01-24 22:12:17 -0300

    Add bootstrap Column into separated component

--- src/components/bootstrap/index.tsx
+++ src/components/bootstrap/index.tsx
@@ -1,9 +1,9 @@
-interface RowProps {
+interface BasicComponentsProps {
     children?: any,
     className?: string,
 }
 
-export const Row = ({ children, className }: RowProps) => {
+export const Row = ({ className, children }: BasicComponentsProps) => {
 
     const cssClasses = ['row'];
 
@@ -15,3 +15,30 @@ export const Row = ({ children, className }: RowProps) => {
         </div>
     );
 };
+
+interface ColProps extends BasicComponentsProps {
+    xs?: number,
+    sm?: number,
+    md?: number,
+    lg?: number,
+    xl?: number,
+    xxl?: number,
+}
+
+export const Col = ({ className, xs, sm, md, lg, xl, xxl, children }:ColProps) => {
+
+    const cssClasses = className ? [ className ] : [];
+
+    if (xs) cssClasses.push(`col-${xs}`);
+    if (sm) cssClasses.push(`col-sm-${sm}`);
+    if (md) cssClasses.push(`col-md-${md}`);
+    if (lg) cssClasses.push(`col-lg-${lg}`);
+    if (xl) cssClasses.push(`col-xl-${xl}`);
+    if (xxl) cssClasses.push(`col-xxl-${xxl}`);
+
+    return (
+        <div className={ cssClasses.join(' ') }>
+            {children}
+        </div>
+    );
+}
