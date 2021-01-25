850fa58ba0e1163df91c9fba671e0f241be85871
Matheus Lopes <matlopes1999@gmail.com>
2021-01-24 22:13:07 -0300

    Added TitleH5 into typografy components

--- src/components/typografy/index.tsx
+++ src/components/typografy/index.tsx
@@ -1,6 +1,6 @@
-import { TitleH1, TitleH2, TitleH3, TitleH4 } from './styledTypografy';
+import { TitleH1, TitleH2, TitleH3, TitleH4, TitleH5 } from './styledTypografy';
 
 /**
  * this titles need a relative parent node couse it use an absolute position.
  */
-export { TitleH1, TitleH2, TitleH3, TitleH4 };
+export { TitleH1, TitleH2, TitleH3, TitleH4, TitleH5 };
--- src/components/typografy/styledTypografy.ts
+++ src/components/typografy/styledTypografy.ts
@@ -75,3 +75,22 @@ export const TitleH4 = styled.h4`
         left: 0;
     }
 `;
+
+export const TitleH5 = styled.h5`
+    color: ${({ theme }) => theme.colors.primary};
+    text-transform: uppercase;
+    font-family: ${({ theme }) => theme.fonts.sourceSansProFont};
+    font-weight: bold;
+    margin-bottom: 0.5em;
+    width: fit-content;
+    position: relative;
+
+    &::after {
+        content: '';
+        width: 60%;
+        border-bottom: 1px solid #A7B6C5;
+        position: absolute;
+        bottom: 0;
+        left: 0;
+    }
+`;
